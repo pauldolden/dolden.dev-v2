3b659fee0d69e96b351eac50c7a45576eaf34a98
Paul <paul@pop-os.localdomain>
2022-01-02 12:42:35 +0000

    z-index issue with scroll to top

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <body className="bg-blob bg-repeat-y bg-fix overflow-x-hidden font-rubik text-white">
+        <body className="bg-blob bg-repeat-y bg-fix transform-gpu overflow-x-hidden font-rubik text-white">
           <Main />
           <NextScript />
         </body>
